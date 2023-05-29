1833230411a9980ecd82ac767f8ddbafcbc34a78
Matheus Bosa <matheusbosa95@gmail.com>
2023-05-29 11:23:04 -0300

    Exception handling - delete

--- src/main/java/com/example/trabalhoSistemaDePonto/services/FuncionarioService.java
+++ src/main/java/com/example/trabalhoSistemaDePonto/services/FuncionarioService.java
@@ -4,15 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.trabalhoSistemaDePonto.entitities.Funcionario;
 import com.example.trabalhoSistemaDePonto.repositories.FuncionarioRepository;
 import com.example.trabalhoSistemaDePonto.services.exceptions.DatabaseException;
 import com.example.trabalhoSistemaDePonto.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class FuncionarioService {
@@ -46,9 +46,13 @@ public class FuncionarioService {
 	}
 
 	public Funcionario update(Long id, Funcionario obj) {
-		Funcionario entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			Funcionario entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(Funcionario entity, Funcionario obj) {
